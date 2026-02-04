432ed30809d70617eb26f8abbff0c8b30834fdef
lizao413njupt <b23130830@njupt.edu.cn>
2026-02-04 11:50:29 +0800

    完成challenge02的第三部分
    初步实现toggle的todo需要到最底下这一功能。但是发现有错误

--- projects/react-demo/react-concise-course/challenge02/main.js
+++ projects/react-demo/react-concise-course/challenge02/main.js
@@ -7,29 +7,69 @@ function MyApp() {
   const [todoList, setTodoList] = React.useState([]);
   const [currentTodo, setCurrentTodo] = React.useState("");
 
+  // 添加todo
   function handleAddTodo() {
     if(!currentTodo.trim().length) {
       alert("请输入一个待办事项")
     }
     const newTodoList = [
       ...todoList,
-      {content:currentTodo}
+      {
+        content:currentTodo,
+        isCompleted:false
+      }
     ]
     setTodoList(newTodoList)
     setCurrentTodo("")
   }
 
+  // 删除todo
   function handleDeleteTodo(id) {
     // 筛选掉所有和传入id相同的
     const deletedTodoList =  todoList.filter((todo, idx) => idx !== id)
     // 每次修改过后都要记得调用set方法更新，不然没用
     setTodoList(deletedTodoList)
   }
+  // 对isCompleted的值进行一个翻转
+  function toggleTodo(id) {
+    const toggledTodoList = todoList.map((todo, idx) => {
+      /* 如果找到了那个要修改的值，那么我们需要返回一个对象，
+      内容是用之前的todo的isCompleted取反，再将其覆盖再原来
+      的todo上
+      */
+      if(idx === id) {
+        
+        return {
+          ...todo,
+          isCompleted:!todo.isCompleted
+        }
+      }
+      // 虽然不太可能找不到，但是还是返回一下原值
+      return todo
+    })
+    /* 我们要求没有toggle的放在toggle的前面，因此每次做完toggle的操作
+    需要就这一要求进行排序
+    */
+    sortTodoList()
+  }
+
+  function sortTodoList(todoList) {
+    const sortedTodoList = todoList.sort((o1, o2) => {
+      if(o1.isCompleted !== o2.isCompleted) {
+        // 返回1代表o1在后边，-1代表前边
+        return o1.isCompleted?1:-1
+      }
+      // 都不是的话返回0
+      return 0
+
+    })
+    setTodoList(sortedTodoList)
+  }
 
   return (
     <main>
       <h1>React Todo List</h1>
-      <input 
+      <input       
         type="text" 
         placeholder="Add item into as todo"
         value={currentTodo}
@@ -41,8 +81,13 @@ function MyApp() {
       <ul>
         {/* map函数可以同时获取两个参数，第一个是item,第二个是参数index */}
         {todoList.length > 0 && todoList.map((todo, idx) =>(
-          <li className="deleted">
-            <input type="checkbox" />
+          // 对className进行一个简单的判断
+          <li className={todo.isCompleted?"deleted":""}>
+            <input 
+              type="checkbox" 
+              // 必须要把函数调用切换为匿名函数，不能写成toggleTodo(idx)的形式
+              onClick={() => toggleTodo(idx)}  
+            />
             {todo.content}
             <button
               onClick={() => handleDeleteTodo(idx)}
